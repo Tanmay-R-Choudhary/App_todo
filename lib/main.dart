732bf5a4353763c58527beeadcb1718b33ddcc71
Tanmay-R-Choudhary <tanmay.d.choudhary@gmail.com>
2021-11-12 16:18:19 +0530

    final app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:medium_app/pages/home.dart';
+
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
